f05bac94a35baaad97c9265add352a382176717d
studioHalf-bottle <68529942+studioHalf-bottle@users.noreply.github.com>
2023-03-09 13:41:28 +0900

    Add files via upload

--- archive/controller.js
+++ archive/controller.js
@@ -1,10 +1,153 @@
 var screen_main = document.getElementsByTagName("main")[0];
-var screen_posting_menu = document.getElementById("posting-menu");
-var screen_detail = document.getElementById("detail-articles");
+var screen_posting_list = document.getElementById("posting-list");
+var screen_detail_articles = document.getElementById("detail-articles");
 
-var uncovering_button = document.getElementById("uncovering-button");
-var showing_fullText_button = document.getElementById("showing-fullText-button");
-var hiding_fullText_button = document.getElementById("hiding-fullText-button");
+var postList_fromJSON = new Array(); 
+
+
+/* ------- bulid initial list of posts ------ */
+function return_postList_fromJSON( _json ) {
+	let array_input = _json; 
+	let array_output = new Array(); 
+	
+	array_input.forEach( function( _categoryEle ) {
+		_categoryEle.post_list.forEach( function( _postEle ) {
+			_postEle.category = _categoryEle.category; 
+			_postEle.category_KR = _categoryEle.category_KR; 
+			_postEle.category_class = _categoryEle.category_class; 
+			array_output.push(_postEle);
+		});
+	});
+	
+	array_output.sort( function( a, b ) {
+		return new Date(a.publishingDate).getTime() - new Date(b.publishingDate).getTime(); 
+	});
+	return array_output;
+}
+
+
+function build_archiveMenu_left() {
+	var head_domElement, metadata_domElement, date_domElement, category_domElement, publisher_domElement, title_domElement; 
+	
+	postList_fromJSON.forEach( function( _postEle ) {
+		
+		postTime = new Date(_postEle.publishingDate);
+		date_domElement = document.createElement("date"); 
+		date_domElement.appendChild( document.createTextNode(`${postTime.getFullYear()}. ${postTime.getMonth()+1}. ${postTime.getDate()} - `) );
+		category_domElement = document.createElement("span"); 
+		category_domElement.classList.add( "posting-category" );
+		category_domElement.appendChild( document.createTextNode(`${_postEle.category_KR}, `) );
+		publisher_domElement = document.createElement("span"); 
+		publisher_domElement.classList.add( "posting-publisher" );
+		publisher_domElement.appendChild( document.createTextNode(`${_postEle.publisher}`) );
+		
+		metadata_domElement = document.createElement("div");
+		metadata_domElement.classList.add( "posting-metadata" );
+		metadata_domElement.append(date_domElement, category_domElement, publisher_domElement);
+		
+		title_domElement = document.createElement("h2"); 
+		title_domElement.classList.add( "posting-title" );
+		title_domElement.appendChild( document.createTextNode(`${_postEle.title}`) );
+		
+		head_domElement = document.createElement("section"); 
+		head_domElement.classList.add( "posting-head", _postEle.category_class ); 
+		head_domElement.append( metadata_domElement, title_domElement );
+		
+		screen_posting_list.appendChild(head_domElement); 
+		
+	});
+	
+}
+
+
+function build_detailArticles_right() {
+	var domParser = new DOMParser();
+	var domDoc_fromParser; 
+	var article_domElement, metadata_domElement, date_domElement, category_domElement, publisher_domElement, title_domElement; 
+	var nav01_domElement, nav02_domElement;
+	var p_domElement_array, p_caption_domElement; 
+	var image_video_domElement;
+	
+	postList_fromJSON.forEach( function( _postEle ) {
+		
+		postTime = new Date(_postEle.publishingDate);
+		date_domElement = document.createElement("date"); 
+		date_domElement.appendChild( document.createTextNode(`${postTime.getFullYear()}. ${postTime.getMonth()+1}. ${postTime.getDate()} `) );
+		category_domElement = document.createElement("span"); 
+		category_domElement.classList.add( "posting-category" );
+		category_domElement.appendChild( document.createTextNode(`${_postEle.category_KR}, `) );
+		publisher_domElement = document.createElement("span"); 
+		publisher_domElement.classList.add( "posting-publisher" );
+		publisher_domElement.appendChild( document.createTextNode(`${_postEle.publisher}`) );
+		
+		metadata_domElement = document.createElement("div");
+		metadata_domElement.classList.add( "posting-metadata" );
+		metadata_domElement.append(date_domElement, document.createElement("br"), category_domElement, publisher_domElement);
+		
+		title_domElement = document.createElement("h2"); 
+		title_domElement.classList.add( "posting-title" );
+		title_domElement.appendChild( document.createTextNode(`${_postEle.title}`) );
+		
+		
+		if (_postEle.hyperlink_01) {
+			nav01_domElement = document.createElement("nav"); 
+			domDoc_fromParser = domParser.parseFromString(`<a href="${_postEle.hyperlink_01}" target="_blank">${_postEle.hyperlink_01_desc} →</a>`, "text/html");
+			nav01_domElement.classList.add( "posting-relatedLink" );
+			nav01_domElement.appendChild( domDoc_fromParser.body.firstChild );
+		}
+		if (_postEle.hyperlink_02) {
+			nav02_domElement = document.createElement("nav"); 
+			domDoc_fromParser = domParser.parseFromString(`<a href="${_postEle.hyperlink_02}" target="_blank">${_postEle.hyperlink_02_desc} →</a>`, "text/html");
+			nav02_domElement.classList.add( "posting-relatedLink" );
+			nav02_domElement.appendChild( domDoc_fromParser.body.firstChild );
+		}
+		
+		p_domElement_array = new Array();
+		_postEle.html_p_list.forEach( function( _pEle ) {
+			domDoc_fromParser = domParser.parseFromString(`<p>${_pEle}</p>`, "text/html");
+			p_domElement_array.push( domDoc_fromParser.body.firstChild );
+		})
+		
+		if (_postEle.html_p_caption) {
+			p_caption_domElement = document.createElement("p"); 
+			domDoc_fromParser = domParser.parseFromString(`${_postEle.html_p_caption}`, "text/html");
+			p_caption_domElement.classList.add( "posting-caption" );
+			p_caption_domElement.appendChild( domDoc_fromParser.body.firstChild );
+		}
+		
+		if (_postEle.html_image_video) {
+			if (_postEle.html_image_video.slice(0, 30) == "https://www.youtube.com/embed/") // YouTube Video
+				domDoc_fromParser = domParser.parseFromString(
+					`<div class="iframe-container">
+							<iframe loading="lazy" width="560" height="315" src="${_postEle.html_image_video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					</div>`
+					, "text/html");
+			else // image
+				domDoc_fromParser = domParser.parseFromString(
+					`<img src="${_postEle.html_image_video}" alt="" loading="lazy" />`
+					, "text/html");
+			image_video_domElement = document.createElement("figure");
+			image_video_domElement.appendChild( domDoc_fromParser.body.firstChild );
+		}
+		
+		
+		article_domElement = document.createElement("article"); 
+		article_domElement.classList.add( "posting-article", _postEle.category_class );
+		article_domElement.append( metadata_domElement, title_domElement );
+		if (_postEle.hyperlink_01) article_domElement.appendChild( nav01_domElement );
+		if (_postEle.hyperlink_02) article_domElement.appendChild( nav02_domElement );
+//		p_domElement_array.forEach( function( _pDomEle ) { 
+//			article_domElement.appendChild( _pDomEle ); 
+//		});
+		article_domElement.append( ...p_domElement_array ); 
+		if (_postEle.html_p_caption) article_domElement.appendChild( p_caption_domElement );
+		if (_postEle.html_image_video) article_domElement.appendChild( image_video_domElement );
+		
+		
+		screen_detail_articles.appendChild( article_domElement ); 
+	});
+	
+}
 
 
 /* ------- main UI/UX functions ------ */
@@ -22,7 +165,7 @@ function letsShowThePosting( _nextHead ) {
 	
 	if (prevDetail != undefined) prevDetail.classList.remove('shown-detail');
 	nextDetail.classList.add('shown-detail');
-	screen_detail.scrollTo(0,0);
+	screen_detail_articles.scrollTo(0,0);
 	
 	screen_main.classList.remove('main-posting-category-data', 'main-posting-category-video', 'main-posting-category-paper', 'main-posting-category-blog', 'main-posting-category-press', 'main-empty-detail');
 	screen_main.classList.add(nextCategoryClass);
@@ -97,20 +240,45 @@ function matchHEAD_CATEGORY( _headEle ) {
 
 
 
-/* ------- DOMContentLoaded ------ */
+
+
+
+/* ------- initial JSON fetch & DOMContentLoaded ------ */
+
 
 // Handler when the DOM is fully loaded, do this.
 document.addEventListener("DOMContentLoaded", function(){
+// Handler when every resource is fully loaded, do this.
+//document.addEventListener("load", function(){
 	var postingHead_collection = document.getElementsByClassName("posting-head");
 	var filterButton_collection = document.getElementsByClassName("category-filter-button");
 	var closeButton = document.getElementById("hiding-detail-article-button");
 	
-	for(let i=0; i<postingHead_collection.length; i++){
-		postingHead_collection[i].addEventListener('click', function(event) {
-			var targetElement = $(event.target).closest(".posting-head")[0];
-			letsShowThePosting( targetElement ); 
-		});
-	}
+	// call the JSON file("archive-post-list.json"), parse it, rearrange it and assign it into variable "postList_fromJSON". 
+	fetch('../@asset/archive-post-list.json')
+			.then((response) => {
+					if (!response.ok) {
+						throw new Error('네트워크 응답이 올바르지 않습니다.');
+					}
+					return response.json(); 
+			})
+			.then(function(_json) { 
+					postList_fromJSON = return_postList_fromJSON(_json);
+					build_archiveMenu_left(); 
+					build_detailArticles_right();
+			})
+			.then(function() { 
+					for(let i=0; i<postingHead_collection.length; i++){
+						postingHead_collection[i].addEventListener('click', function(event) {
+							var targetElement = $(event.target).closest(".posting-head")[0];
+							letsShowThePosting( targetElement ); 
+						});
+					}
+			})
+			.catch((error) => {
+					console.error('fetch에 문제가 있었습니다.', error);
+			});
+	
 	for(let i=0; i<filterButton_collection.length; i++){
 		filterButton_collection[i].addEventListener('click', function(event) {
 			var targetElement = $(event.target).closest(".category-filter-button")[0];
